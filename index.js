41c83e77e9419c5cffc404447ca7abe50659b5df
Diego ruan <diego_ruan.14@hotmail.com>
2023-12-22 21:44:09 -0300

    Aula JavaScript: programação orientada a objetos

---
+++ index.js
@@ -0,0 +1,14 @@
+import User from "./User.js";
+import Docente from "./Docente.js";
+import Admin from "./admin.js";
+
+// const novoUser = new User("Diego", "d@d.com", "2000-07-19");
+// console.log(novoUser.nome);
+
+// const novoAdmin = new Admin("Rodrigo", "r@r.com", "2021-01-01");
+// console.log(novoAdmin.exibirInfos());
+// novoAdmin.nome = "Diego";
+// console.log(novoAdmin.nome);
+
+// const novoDocente = new Docente("Guilherme", "g@g.com", "2021-01-01");
+// console.log(novoDocente.exibirInfos());
